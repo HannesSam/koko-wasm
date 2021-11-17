b1f41395d5904c1ee99cf344820b94e7cb44687a
Hannes Samskog <hannes.samskog@omegapoint.se>
2021-11-17 19:51:32 +0100

    More layout

--- lab2/src/App.tsx
+++ lab2/src/App.tsx
@@ -1,8 +1,29 @@
+import { Button, Col, Container, FormControl, InputGroup, Row } from "react-bootstrap";
+import CarouselComponent from "./Components/CarouselComponent";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
   return (
-    <div className="App">
-      <p>Ojoj här kommer det hända grejer</p>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <CarouselComponent />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h2>Who are you quiz</h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <InputGroup className="mb-3">
+            <FormControl placeholder="Charachter traits (Comma seperated list)" />
+            <Button variant="outline-secondary">Search</Button>
+          </InputGroup>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
---
+++ lab2/src/Components/CarouselComponent.tsx
@@ -0,0 +1,29 @@
+import { Carousel } from "react-bootstrap";
+
+export default function CarouselComponent() {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img className="d-block w-100" src="/img/gollum.jpg" alt="First slide" />
+        <Carousel.Caption>
+          <h3>Are you gready?</h3>
+          <p>Then maybe your gollum</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src="/img/harry.jpg" alt="Second slide" />
+        <Carousel.Caption>
+          <h3>Are you humble?</h3>
+          <p>Then you might be a wizard harry</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src="/img/minions.jpg" alt="Third slide" />
+        <Carousel.Caption>
+          <h3>Do you like being part of a group?</h3>
+          <p>Then you might be a minion</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
