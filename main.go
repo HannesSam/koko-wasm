76b495ae2f1b9d49695ceaeacd3b2158476ae410
Hannes Samskog <hannes.samskog@omegapoint.se>
2021-11-18 22:51:46 +0100

    Updates readme

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"time"
+)
+
+// This calls a JS function from Go.
+func main() {
+	//println("Pi ", monteCarloPi(500000000))
+}
+
+// This function is exported to JavaScript, so can be called using
+// exports.multiply() in JavaScript.
+func multiply(x, y int) int {
+	return x * y
+}
+
+func getTime() uint64 {
+	return uint64(time.Now().UnixNano())
+}
+
+var x uint64 = getTime()
+
+//export randomNumber
+func randomNumber() float64 {
+	const a = uint64(1013904223)
+	const m = uint64(1664525)
+	const max = uint64(1 << 32)
+	x = (m*x + a) % max
+	return float64(x) / float64(max)
+}
+
+// TinyGo looks at this comment to determine what to export.
+//export monteCarloPi
+func monteCarloPi(iterations uint32) float64 {
+	var inside uint64
+	for i := uint32(0); i < iterations; i++ {
+		x := randomNumber()
+		y := randomNumber()
+		if x*x+y*y < 1.0 {
+			inside++
+		}
+	}
+	return 4 * float64(inside) / float64(iterations)
+}
